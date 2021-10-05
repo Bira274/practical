82b50e9b132eec7af86858fbc5a8e42e15847dd7
Sabrina Birabwa <89991227+Bira274@users.noreply.github.com>
2021-10-04 20:28:38 -0700

    First js practice

---
+++ intro.js
@@ -0,0 +1,48 @@
+// function showData(name, age) {
+//     console.log(name, age)
+// }
+
+// showData ('Leah', 20)
+// showData('Florence', 50)
+// showData('Dan', 10)
+// showData('Rachel', 30)
+// showData('Francis', 40)
+
+// function calculateDistance(speed, time){
+//     console.log(speed*time) 
+// }
+// function displayCost(name, speed, time){
+    
+//     cost=150*calculateDistance(speed,time)
+//     console.log(speed,'m/min', time,'mins')
+//     console.log(name,'please pay UGX',cost)
+// }
+// calculateDistance(50, 30)
+// displayCost('Leah', 50, 30)
+
+// let  x=10
+// let y=2
+// z = (x,y) => x/y; console.log(x,'divided by',y,'is equal to',z(x,y),)
+
+
+// for (let a=1; a<20; a++) {
+//     console.log('I love Uganda',a)
+// }
+// function calculateProfit(cost) {
+//     let sellingPrice= 10000
+//     console.log(profit=sellingPrice-cost)
+// }
+// function displayProfit(cost) {
+//     console.log(calculateProfit(cost))
+// }
+// calculateProfit(5000)
+
+function calculateCost(speed, time) {
+    let fare=5
+    console.log(cost= speed*time*fare)
+}
+function displayCost() {
+    console.log('Please pay UGX',calculateCost(speed, time))
+}
+calculateCost(50, 10)
+displayCost()
